c6ca675e56dfb6ccebeb5ac6f7bd807826a5c075
gscheema <gurpreet.singh@ajdevtech.com>
2025-05-29 17:56:51 +0530

    `Refactor code in array/lists.go: update variable declarations, append to slice, and print slice elements`

--- array/lists.go
+++ array/lists.go
@@ -9,11 +9,25 @@ type Product struct {
 }
 
 func main() {
-	var productNames [4]string
-	productNames = [4]string{"Product 1", "Product 2", "Product 3", "product 4"}
-	prices := [4]float64{10.99, 9.99, 12.99, 11.99}
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+	prices = append(prices, 5.99)
 	fmt.Println(prices)
-	fmt.Println(productNames[2])
-	// titles  := [4]string {"Product A", "Product B", "Product C", "Product D"}
-	fmt.Println(prices[3])
 }
+
+// func main() {
+// 	var productNames [4]string
+// 	productNames = [4]string{"Product 1", "Product 2", "Product 3", "product 4"}
+// 	prices := [5]float64{10.99, 9.99, 12.99, 11.99, 12.78}
+// 	fmt.Println(prices)
+// 	fmt.Println(productNames[2])
+// 	// titles  := [4]string {"Product A", "Product B", "Product C", "Product D"}
+// 	fmt.Println(prices[3])
+
+// 	featuredPrices := prices[1:]
+// 	fmt.Println(featuredPrices)
+// 	highlighrdPrices := featuredPrices[:1]
+
+// 	fmt.Println(highlighrdPrices)
+// }
