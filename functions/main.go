764d8242651230192c834ae2de0b7e41f4ff5f14
gscheema <gurpreet.singh@ajdevtech.com>
2025-05-30 13:40:31 +0530

    `Refactor transform functions and remove unused code`

--- functions/main.go
+++ functions/main.go
@@ -2,57 +2,37 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 2, 1}
+	numbers := []int{1, 2, 3}
+	double := createTransformer(2)
+	tripple := createTransformer(3)
+
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
+
 	doubled := transformNumbers(&numbers, double)
 	trippled := transformNumbers(&numbers, tripple)
-	fmt.Println(doubled)
-	fmt.Println(trippled)
-	transformFn1 := getTransformerFunction(&numbers)
-	transformFn2 := getTransformerFunction(&moreNumbers)
+	fmt.Println("Original:", numbers)
+	fmt.Println("Transformed:", transformed)
+	fmt.Println("Doubled:", doubled)
+	fmt.Println("Tripled:", trippled)
 
-	transformedNumbers := transformNumbers(&numbers, transformFn1)
-	moreTransformedNumbers := transformNumbers(&moreNumbers, transformFn2)
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
+	fmt.Println(transformed)
 }
 
-func doubleNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
+
 	for _, val := range *numbers {
-		dNumbers = append(dNumbers, multiplyNumber(val, 4))
+		dNumbers = append(dNumbers, transform(val))
 	}
+
 	return dNumbers
 }
 
-func transformNumbers(numbers *[]int, trasform transformFn) []int {
-	dNumbers := []int{}
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, trasform(val))
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
 	}
-	return dNumbers
-}
-
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return tripple
-	}
-
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func tripple(number int) int {
-	return number * 3
-}
-
-func multiplyNumber(number, val int) int {
-	return number * val
 }
